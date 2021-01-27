e2e741593a8783bf85e299677e237d76358b846d
WayneCarl Barker <wakywayne80@gmail.com>
2021-01-27 15:54:45 -0500

    Part 1 of lab Completed

---
+++ app.js
@@ -0,0 +1,10 @@
+friends = ["Alex", "Branden", "Brown", "Jason", "Jermaine",];
+
+for(let a = 0; a < 5; a++){
+    let b = 0;
+    
+    while(b < 100){
+        console.log("I said this "+b+" times "+friends[a]);
+        b++;
+    }
+}
